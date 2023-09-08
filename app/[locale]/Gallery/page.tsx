512558dc0f22fef16bb9d1fe3271b5e8a5b185d6
Milltiades <gigakakulia2017@gmail.com>
2023-09-08 11:13:26 +0400

    deleted slider component

--- app/[locale]/Gallery/page.tsx
+++ app/[locale]/Gallery/page.tsx
@@ -1,11 +1,7 @@
 "use client";
-import SliderComponent from "@/components/SliderComponent.tsx";
+
 import React from "react";
 
 export default function page() {
-  return (
-    <div>
-      <SliderComponent />
-    </div>
-  );
+  return <div>Gallery page</div>;
 }
