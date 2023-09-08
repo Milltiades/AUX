177aa536565d6c829ce567865c9f0c70f2df1b06
Milltiades <gigakakulia2017@gmail.com>
2023-09-08 10:52:46 +0400

    add margint to title in about page

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -7,7 +7,7 @@ export default function page() {
   const t = useTranslations("About");
   return (
     <div className="pt-8 pb-16 flex flex-col px-5 sm:px-5 lg:px-48 xl:px-56 2xl:px-60 min-h-custom">
-      <h1 className=" mt-5 text-xl sm:text-2xl">{t("title")}</h1>
+      <h1 className=" mt-8 text-xl sm:text-2xl">{t("title")}</h1>
       <p className=" mt-5 text-sm sm:text-xl">{t("text")}</p>
     </div>
   );
